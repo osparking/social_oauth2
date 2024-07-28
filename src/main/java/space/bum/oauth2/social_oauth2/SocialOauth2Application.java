02832f2e88222f19eaf85ea406584589dbe36487
osparking <jbpark03@gmail.com>
2024-07-28 10:33:32 +0900

    Social OAuth2 study 2nd exercise - initial state

---
+++ src/main/java/space/bum/oauth2/social_oauth2/SocialOauth2Application.java
@@ -0,0 +1,13 @@
+package space.bum.oauth2.social_oauth2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialOauth2Application.class, args);
+	}
+
+}
